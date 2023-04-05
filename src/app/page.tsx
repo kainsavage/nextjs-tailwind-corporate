2271603c7eb11b7c972cef94c527e6ae86bd2793
kain <kain@teamclerks.net>
2023-04-05 11:41:34 -0700

    hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
 export default function Home() {
-  return <div className="text-green-300 text-9xl font-sans">Home</div>;
+  return (
+    <div className="flex flex-col flex-grow bg-[url('/shaking-hands.jpg')] bg-no-repeat bg-cover rounded-3xl">
+      <div className="flex text-black text-center text-8xl font-bold mx-auto mt-12 mb-10">
+        Trust us with
+        <br />
+        your business
+      </div>
+      <button className="flex mx-auto bg-gradient-to-t from-red-600 to-red-800 px-4 py-2 rounded-md text-slate-200 font-semibold mb-96">
+        Customer Center
+      </button>
+    </div>
+  );
 }
