93dc919db5a038084898268ba354883856899875
kain <kain@teamclerks.net>
2023-04-06 08:43:45 -0700

    loading/use() example

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,25 @@
+import { use } from 'react';
 import Tributton from './components/Tributton';
 
+function sleep() {
+  return new Promise((resolve) => setTimeout(resolve, 5000));
+}
+
+async function mock_rest() {
+  await sleep();
+  return 50;
+}
+
 export default function Home() {
+  const foo = use(mock_rest());
+
   return (
     <>
       <div className="flex flex-col flex-grow bg-[url('/shaking-hands.jpg')] bg-no-repeat bg-cover rounded-3xl">
         <div className="flex text-black text-center text-8xl font-bold mx-auto mt-12 mb-10">
           Trust us with
           <br />
-          your business
+          your business {foo}
         </div>
         <button className="flex mx-auto bg-gradient-to-t from-red-600 to-red-800 px-4 py-2 rounded-md text-slate-200 font-semibold mb-96">
           Customer Center
